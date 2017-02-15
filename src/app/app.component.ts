f3833b1b51b7da847eb9f70e20d1f0e00254d041
lushien <lushien@ecan.co.nz>
2017-02-15 13:14:08 +1300

    Setup notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-
+import {Push} from 'ionic-native';
 import { TabsPage } from '../pages/tabs/tabs';
 
 
@@ -10,13 +10,62 @@ import { TabsPage } from '../pages/tabs/tabs';
 })
 export class MyApp {
   rootPage = TabsPage;
-
+  
   constructor(platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+      
+      var push = Push.init({
+        android: {
+          senderID: "XXXXXXXXX"
+        },
+        ios: {
+          alert: "true",
+          badge: true,
+          sound: 'false'
+        },
+        windows: {}
+      });
+      push.on('registration', (data) => {
+        console.log(data.registrationId);
+        alert(data.registrationId.toString());
+      });
+      push.on('notification', (data) => {
+        console.log(data);
+        alert("Hi, Am a push notification");
+      });
+      push.on('error', (e) => {
+        console.log(e.message);
+      });
+      this.initialisePushing();
     });
+  }
+
+  initialisePushing(){
+    // var push = Push.init({
+    //     android: {
+    //       senderID: "XXXXXXXXX"
+    //     },
+    //     ios: {
+    //       alert: "true",
+    //       badge: true,
+    //       sound: 'false'
+    //     },
+    //     windows: {}
+    //   });
+    //   push.on('registration', (data) => {
+    //     console.log(data.registrationId);
+    //     alert(data.registrationId.toString());
+    //   });
+    //   push.on('notification', (data) => {
+    //     console.log(data);
+    //     alert("Hi, Am a push notification");
+    //   });
+    //   push.on('error', (e) => {
+    //     console.log(e.message);
+    //   });
   }
 }
