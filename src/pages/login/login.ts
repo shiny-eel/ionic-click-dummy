5713a1a3de0184a9ea01f2d8ccc678adcc7f3b16
lushien <lushien@ecan.co.nz>
2017-02-22 12:34:33 +1300

    Add 'save login' dialogue

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Events } from 'ionic-angular';
 
-import { NavController, ViewController } from 'ionic-angular';
+import { NavController, ViewController, AlertController } from 'ionic-angular';
 
 import { SignupPage } from '../signup/signup';
 import { TabsPage } from '../tabs/tabs';
 import { UserData } from '../../providers/user-data';
+import { LoginDetails } from '../../providers/user-data';
+
 import { StorageAccess } from '../../providers/storage-access';
 
 import { AccountPage } from '../account/account';
@@ -17,35 +19,48 @@ import { AccountPage } from '../account/account';
 	templateUrl: 'login.html'
 })
 export class LoginPage {
-	login: { username?: string, password?: string } = {};
+	login: LoginDetails = new LoginDetails(null, null);
 	submitted = false;
 	invalidLogin = false;
+	askToSaveLogin = true;
 
 	constructor(public navCtrl: NavController, public userData: UserData,
 		public viewCtrl: ViewController,
 		public events: Events,
-		public storageAccess: StorageAccess) {
+		public storageAccess: StorageAccess,
+		private alertCtrl: AlertController) {
 
 		this.events.subscribe('user:invalid', (event) => {
 			console.log('InvalidLogin, keep logon page open');
-			//this.goToLogin();
 			this.invalidLogin = true;
+
 		});
+
+		this.events.subscribe('user:login', (event) => {
+			console.log('User Login, ask to save login');
+			this.presentConfirm();
+		});
+
 		let myself = this;
-		this.storageAccess.get('login').then(
+		this.storageAccess.get(this.storageAccess.LOGIN_KEY).then(
 			data => {
-				console.log('found data from storage for login', data);
-				myself.login.username = data.username
-				myself.login.password = data.password
-				
+				console.log('found data from storage for login\n', data);
+				// Need to add type safety here TODO
+				myself.login = JSON.parse(data);
+				console.log(myself.login);
+				myself.askToSaveLogin = true;
+
 			},
-			reject => console.log(reject)
+			reject => {
+				console.log('Did not find login in storage\n', reject)
+				myself.askToSaveLogin = false;
+
+			}
 		)
 	}
 
 	onLogin(form: NgForm) {
 		this.submitted = true;
-
 		if (form.valid) {
 			this.userData.login(this.login.username, this.login.password);
 			//   this.navCtrl.push(AccountPage);
@@ -54,5 +69,29 @@ export class LoginPage {
 
 	onSignup() {
 		this.navCtrl.push(SignupPage);
+	}
+
+	presentConfirm() {
+		let alert = this.alertCtrl.create({
+			title: 'Remember You',
+			message: 'Do you want us to remember your login deets?',
+			buttons: [
+				{
+					text: 'No Thanks',
+					role: 'cancel',
+					handler: () => {
+						console.log('Cancel clicked');
+					}
+				},
+				{
+					text: 'Remember Me!',
+					handler: () => {
+						console.log('Buy clicked');
+						this.userData.saveLogin(this.login);
+					}
+				}
+			]
+		});
+		alert.present();
 	}
 }
